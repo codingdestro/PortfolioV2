d7c8c79e2fafd7fd189f7e57956c9c9a337998bd
codingdestro <ma1637285@gmail.com>
2024-07-25 11:39:20 +0530

    add - useGetContributions hook

--- src/components/contribution/TotalContributionCard.tsx
+++ src/components/contribution/TotalContributionCard.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect, useRef } from "react"
 import axios from "axios"
 import { useView } from "../../hooks/useView"
+import { useGetContribution } from "../../hooks/useGetContribution"
 
 const TotalContributionCard = () => {
   const [repoCount, setRepoCount] = useState(0)
   const divRef = useRef<HTMLDivElement>(null)
   const { isVisible } = useView(divRef)
+  const { count, isLoading } = useGetContribution()
 
   useEffect(() => {
     ; (async () => {
@@ -29,7 +31,7 @@ const TotalContributionCard = () => {
       <div className="border min-h-[12rem] font-poppins border-black rounded-md p-5 flex items-center justify-around gap-x-3 shadow-lg animate-fadezoomin">
         <div className="text-center flex-1">
           <h4 className="text-xxl text-ternary font-bold tracking-wider">
-            126
+            {isLoading ? "0" : count}
           </h4>
           <p className="text-slg font-semibold">total contribution</p>
         </div>
---
+++ src/hooks/useGetContribution.ts
@@ -0,0 +1,74 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const GITHUB_TOKEN = "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"
+const GITHUB_GRAPHQL_URL = "https://api.github.com/graphql"
+
+const createQuery = (year: number) => {
+  const from = new Date(year, 0, 1).toISOString()
+  const to = new Date(year, 11, 31).toISOString()
+
+  const query = `
+query {
+  user(login: "codingdestro") {
+    name
+    contributionsCollection(from: "${from}", to: "${to}") {
+      contributionCalendar {
+        colors
+        totalContributions
+        weeks {
+          contributionDays {
+            color
+            contributionCount
+            date
+            weekday
+          }
+          firstDay
+        }
+      }
+    }
+  }
+}`
+  return query
+}
+
+const fetchGithubData = async (year: number) => {
+  try {
+    const query = createQuery(year)
+    const response = await axios.post(
+      GITHUB_GRAPHQL_URL,
+      { query },
+      {
+        headers: {
+          Authorization: `Bearer ${GITHUB_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      },
+    )
+    const count =
+      response.data.data.user.contributionsCollection.contributionCalendar
+        .totalContributions
+    return count
+  } catch (error) {
+    console.error("Error fetching data from GitHub:", error)
+    return 0
+  }
+  return 0
+}
+
+export const useGetContribution = () => {
+  const [isLoading, setLoading] = useState(false)
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    ; (async function() {
+      setLoading(true)
+      let totalContributions = 0
+      for (let year = 2022; year <= new Date().getFullYear(); year++) {
+        totalContributions += await fetchGithubData(year)
+      }
+      setCount(totalContributions)
+      setLoading(false)
+    })()
+  }, [])
+  return { count, isLoading }
+}
