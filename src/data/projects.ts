f7c39870b35834d4eee69795cb7a82da78bc99a1
codingdestro <ma1637285@gmail.com>
2024-08-15 18:33:17 +0530

    update - add gadgetbucket project url

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,14 +11,14 @@ export const projects: PropjectInterface[] = [
     title: "PortfolioV2",
     description: "this is my portfolio v2",
     tags: ["React.js", "Typescript"],
-    visitLink: "#",
+    visitLink: "https://portfolio-seven-tau-91.vercel.app/",
   },
   {
     imgUrl: "/gadgetweb.png",
     title: "Gadget Web",
     description: "Full stack E-commerce web app.",
     tags: ["React.js", "Bun.js"],
-    visitLink: "#",
+    visitLink: "https://gadgetbucket-frontend.vercel.app/",
   },
   {
     imgUrl: "/pdf-editor.png",
